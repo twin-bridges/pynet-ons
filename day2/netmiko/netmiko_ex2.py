c2203333899d06c9599ddfffc2a6e57a27723c5c
Kirk Byers <ktbyers@twb-tech.com>
2020-07-15 14:48:01 -0700

    Netmiko exercises

--- day2/netmiko/netmiko_ex2.py
+++ day2/netmiko/netmiko_ex2.py
@@ -9,27 +9,30 @@ def main():
     """Exercises using Netmiko"""
     passwd = getpass("Enter password: ")
 
-    nxos1 = {
-        "device_type": "cisco_nxos",
-        "host": "nxos1.lasthop.io",
-        "username": "pyclass",
+    sros4 = {
+        "device_type": "nokia_sros",
+        "host": "sros.lasthop.io",
+        "username": "admin",
         "password": passwd,
+        "port": 2214,
     }
 
-    cfg_commands = ["logging console 3"]
+    cfg_commands = ['/configure system location "San Francisco"']
 
-    for a_device in [nxos1]:
+    for a_device in [sros4]:
         net_connect = ConnectHandler(**a_device)
         print("Current Prompt: " + net_connect.find_prompt())
 
-        print("\nConfiguring logging")
+        print("\nConfiguring...")
         print("#" * 80)
         output = net_connect.send_config_set(cfg_commands)
+        save_output = net_connect.save_config()
         print("\nSaving config to startup")
-        net_connect.save_config()
         print(output)
+        print(save_output)
         print("#" * 80)
         print()
+        net_connect.disconnect()
 
 
 if __name__ == "__main__":
