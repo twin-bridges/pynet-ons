f929760063c3191e15bd096dffb3e4ab10a68427
Kirk Byers <ktbyers@twb-tech.com>
2020-07-16 13:29:33 -0700

    Jinja2

---
+++ jinja2_example/jinja2_env.py
@@ -0,0 +1,12 @@
+from jinja2 import FileSystemLoader, StrictUndefined
+from jinja2.environment import Environment
+
+env = Environment(undefined=StrictUndefined)
+env.loader = FileSystemLoader([".", "./templates/"])
+
+my_vars = {"bgp_as": 22, "router_id": "1.1.1.1", "peer1": "10.20.30.1"}
+
+template_file = "bgp_config.j2"
+template = env.get_template(template_file)
+output = template.render(**my_vars)
+print(output)
---
+++ jinja2_example/jinja2_simple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import jinja2
+
+my_dict = {"a": "whatever"}
+
+my_template = """
+some
+text
+of
+something
+{{ a }}
+something
+"""
+
+t = jinja2.Template(my_template)
+print(t.render(my_dict))
